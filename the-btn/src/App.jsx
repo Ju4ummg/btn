dcac66eaa19cf3025a4c676c4bee703c5f1659b7
João Pedro Moraes Guedes <joaopedromoraesguedes@gmail.com>
2024-09-30 16:45:57 -0300

    Adição maior cliques

--- the-btn/src/App.jsx
+++ the-btn/src/App.jsx
@@ -1,4 +1,5 @@
 import Button from "./Button.jsx"
+//import "./styles.css"
 function App() {
   return(
     <>
--- the-btn/src/Button.jsx
+++ the-btn/src/Button.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
-//((100)! / (100-x)!) / 100^x
+import "./styles.css"
+
 function Button(){
     const [probability, setProbability] = useState(0);
     const [reset, setReset] = useState(false);
+    const [maxClicks, setMaxClicks] = useState(0);
+    
   const handleClick = () => {
     const randomNumber = Math.random() * 100; // Gera um número aleatório entre 0 e 100
     if (randomNumber < probability) {
@@ -11,13 +14,20 @@ function Button(){
     } else {
       setReset(false); // Não reseta
       setProbability(prev => Math.min(prev + 1, 100)); // Aumenta a probabilidade até 100%
+      
+      if (probability >= maxClicks){
+        setMaxClicks(probability + 1);
+      }
     }
+
   };
+  
   return (
     <div>
       <button onClick={handleClick}>Clique aqui</button>
       {reset && <p>Botão resetado!</p>}
       <p>Cliques: {probability} </p>
+      <p>Maior quantidade de cliques: {maxClicks}</p>
     </div>
   );
 }
