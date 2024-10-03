248b49998e83de9e8d5e4fdeaeeebcbabc177d5b
João Pedro Moraes Guedes <joaopedromoraesguedes@gmail.com>
2024-10-02 22:42:06 -0300

    Correção bug máximo de cliques

--- the-btn/src/Button.jsx
+++ the-btn/src/Button.jsx
@@ -2,37 +2,34 @@ import { useState } from "react";
 import "./styles.css"
 
 function Button(){
-    const [probability, setProbability] = useState(0);
-    const [reset, setReset] = useState(false);
-    const [maxClicks, setMaxClicks] = useState(0);
-    
+  const [clicks, setClicks] = useState(0);
+  const [probability, setProbability] = useState(0);
+  const [reset, setReset] = useState(false);
+  const [maxClicks, setMaxClicks] = useState(0);
+
   const handleClick = () => {
     const randomNumber = Math.random() * 100; // Gera um número aleatório entre 0 e 100
-    if (reset){
-      setProbability(0);
-    }
-    
+
     if (randomNumber < probability) {
       setReset(true); // Reseta se a probabilidade for atingida
-      //setProbability(0); // Reseta a probabilidade
+      setProbability(0); // Reseta a probabilidade
+      setMaxClicks(prevMax => Math.max(prevMax, clicks));
+      setClicks(0); // Reseta os cliques
     } else {
       setReset(false); // Não reseta
+      setClicks(prev => prev + 1); // Incrementa cliques
       setProbability(prev => Math.min(prev + 1, 100)); // Aumenta a probabilidade até 100%
-      
-      if (probability > maxClicks){
-        setMaxClicks(probability);
-      }
     }
-
   };
-  
+
   return (
     <div>
       <button onClick={handleClick}>Clique aqui</button>
       {reset && <p>Botão resetado!</p>}
-      <p>Cliques: {probability} </p>
+      <p>Cliques: {clicks}</p>
       <p>Maior quantidade de cliques: {maxClicks}</p>
     </div>
   );
 }
+
 export default Button;
